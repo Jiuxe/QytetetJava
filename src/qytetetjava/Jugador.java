0f1c27050455fcf76f8550f0acb13874057ed9c6
Antonio Godoy Gonzalez <43647734+AntonioGodoyUGR@users.noreply.github.com>
2019-10-15 11:15:37 +0200

    Fixed devolverCartaLibertad

--- src/qytetetjava/Jugador.java
+++ src/qytetetjava/Jugador.java
@@ -56,9 +56,9 @@ public class Jugador {
         return false;
     }
     
-    /*Sorpresa devolverCartaLibertad(){     //------------IDK-----------------
-        
-    }*/
+    Sorpresa devolverCartaLibertad(){
+        cartaLibertad = null;
+    }
     
     void irACarcel(Casilla casilla){
         
