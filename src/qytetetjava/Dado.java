9265bb4fe52d086dddb8498a3ea7269185034aac
Nikita Stetskiy <nikin929@gmail.com>
2020-10-09 14:06:40 +0200

    Un cuarto de clase

--- src/qytetetjava/Dado.java
+++ src/qytetetjava/Dado.java
@@ -1,10 +1,26 @@
 package qytetetjava;
 
+import java.util.Random;
 
 public class Dado {
+
+    static final private Dado instance = new Dado();
+    static final private int SalidaCarcel = 5;
     
-    int tirar(){
-         
-        return Math.random()*6+1;
-    }    
+    Random random;
+    int ultimoResultado;
+    boolean debug;
+  
+    private Dado(){
+        ultimoResultado=0;
+        random= new Random();
+        debug=false;
+    }
+
+    int tirar (){
+        if(debug)
+            return ultimoResultado= 1;
+        else
+            return ultimoResultado= 1+random.nextInt(6);
+    }
 }
