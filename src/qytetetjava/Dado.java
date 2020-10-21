b44e1ea371efb4f1bb87a7047b05052f1561a146
Nikita Stetskiy <nikin929@gmail.com>
2020-10-21 17:59:19 +0200

    Arreglo un poco la clase Dado

--- src/qytetetjava/Dado.java
+++ src/qytetetjava/Dado.java
@@ -17,10 +17,20 @@ public class Dado {
         debug=false;
     }
 
+    public static Dado getInstance() {
+        return instance;
+    }
+
     int tirar (){
         if(debug)
             return ultimoResultado= 1;
         else
             return ultimoResultado= 1+random.nextInt(6);
+    }
+
+    boolean salgoDeLaCarcel(){
+        int n = tirar();
+        boolean x = n >= 5 ? true : false;
+        return x;
     }
 }
