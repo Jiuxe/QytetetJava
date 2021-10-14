67bf1422b7bdf71f74d90d239eaedecfc6023cc4
davidteitor <92325973+davidteitor@users.noreply.github.com>
2021-10-14 20:14:47 +0200

    Method to define a car
    Hope it can help you

--- src/qytetetjava/MetodoSalirCarcel.java
+++ src/qytetetjava/MetodoSalirCarcel.java
@@ -3,3 +3,37 @@ package qytetetjava;
 public enum MetodoSalirCarcel {
     TIRANDODADO, PAGANDOLIBERTAD  
 }
+
+
+public class Coche {
+    private int year;
+    private String brand;
+    private int price;
+    private int number;
+
+    private Coche(String b, int year, int price, int number){
+        year = year;
+        brand = b;
+        price = price;
+        number = number;      
+    }
+
+    private int get_year()                {
+        return year;
+    }
+
+    private String get_brand(){
+        return brand;
+    }
+    
+    private void set_brand(new_b){
+        brand = new_b;
+    }
+
+    private int get_price(){
+        return price_model;
+    }
+
+    private int get_number(){
+        return number;
+    }
