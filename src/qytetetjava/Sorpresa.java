be963ff12a0c7d8881e62d29c2e62bd8fbccddc2
davidteitor <92325973+davidteitor@users.noreply.github.com>
2021-10-14 20:01:10 +0200

    Shock class

--- src/qytetetjava/Sorpresa.java
+++ src/qytetetjava/Sorpresa.java
@@ -1,5 +1,10 @@
 package qytetetjava;
 
+/*For working shock*/
+
+package com.unimongame.attackOld;
+import com.unimongame.Unimon;
+
 public class Sorpresa {
    
     package String texto;
@@ -16,4 +21,23 @@ public class Sorpresa {
     public String toString(){
         return "Sorpresa{" + "texto=" + texto + ", valor=" + Integer.toString(valor) + ", tipo=" + tipo + "}";
     }
+}
+
+
+
+
+/*Define class*/
+public class Shock extends Attack {
+
+	public Shock() {
+		super("Im shocking", "wow stop the shock");
+	}
+
+   /*Override method*/
+	@Override
+	public void doAttack(Unimon attacker, Unimon target) {
+		target.reduceHp(30);
+		target.distract();
+	}
+
 }
